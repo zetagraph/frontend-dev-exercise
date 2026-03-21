0254b95569a3cc5ad5229bb3af61d219076d6523
Andrei Zvonkov <>
2026-03-20 20:43:57 -0400

    added header markup

--- new-page/src/components/Header.tsx
+++ new-page/src/components/Header.tsx
@@ -1,13 +1,31 @@
+import envelope from "../assets/envelop.svg";
+import logo from "../assets/logo.svg";
 import styles from "./Header.module.css";
 
-interface HeaderProps {
-  children: React.ReactNode;
-}
-
-export const Header = ({ children }: HeaderProps) => {
+export const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="wrapper">{children}</div>
+      <div className={styles.header__inner}>
+        <a className={styles.logo} href="/" aria-label="Neon home">
+          <img src={logo} alt="Neon Logo" />
+        </a>
+
+        <div className={styles.notifyField}>
+          <form className={styles.notifyForm}>
+            <label className="srOnly" htmlFor="email">
+              Email me when Neon launches
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Email me when Neon launches..."
+            />
+            <button type="submit" aria-label="Submit email">
+              <img src={envelope} alt="" aria-hidden="true" />
+            </button>
+          </form>
+        </div>
+      </div>
     </header>
   );
 };
